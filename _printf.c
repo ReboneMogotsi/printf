76cc12aad47a7391c02920212c2c2a96b20354a5
Rebone Mogotsi <rebone.mogotsi@gmail.com>
2023-08-20 09:19:53 -0700

    output format

--- _printf.c
+++ _printf.c
@@ -1,19 +1,53 @@
+#include "main.h"
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * _printf - A function that produces output according to a format.
- * Return: The number of characters printed.
+ * _printf - A function that produces output to a format.
+ * Return: The number of characters. excl. NULL
+ * @Format: A character string.
  */
 
 int _printf(const char *format, ...)
 {
-	int age = 30;
-	char alphabet = 'R';
-	char* text = "We are passing this project by force";
-
-	printf("%d\n", age);
-	printf("%c\n", alphabet);
-	printf("%s\n", text);
-
-	return (0);
+	int number = 0;
+	va_list list_s;
+	va_start(list_s, format)
+		while(*format)
+		{
+			if(*format != %)
+			{
+				write(1, format, 1);
+				number++;
+			}
+			else
+			{
+				format++;
+				if(*format == '%')
+				{
+					write(1, format,1);
+					number++;
+				}
+				else if(*format == 'c')
+				{
+					char c = va_arg(list_s, int);
+					write(1,&c, );
+					number++;
+				}
+				else if(*format == 's')
+				{
+					char *str = va_arg(list_s,char*)
+						int str-len = 0;
+					while(str[strlen] != '\0'
+							str-len++;
+							write(1, str, str-len);
+							number += str-len;
+				}
+			}
+			format++;
+		}
+		va_end(list_s);
+		return number;
 }
+
+	
